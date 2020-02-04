2929efb74421e254097be743154ac4e29de8cf2e
Brian Thornton <Psutron@gmail.com>
2020-02-03 20:50:05 -0500

    Init

---
+++ build/precache-manifest.1fb916dc86842fc5ec4d70355b9a6bc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a631dd46b97c168a94019d0a6ef0f33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa7e50c6fd7ff8035cdf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fff70265ae8714fb1fe8",
+    "url": "/static/js/2.337d3391.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.337d3391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa7e50c6fd7ff8035cdf",
+    "url": "/static/js/main.eb55b8a5.chunk.js"
+  },
+  {
+    "revision": "7570e8d6d5cfcc0f387d",
+    "url": "/static/js/runtime-main.066e3e80.js"
+  },
+  {
+    "revision": "a56d6d7f176045e6314f92e5a75c92eb",
+    "url": "/static/media/dad.a56d6d7f.png"
+  }
+]);
